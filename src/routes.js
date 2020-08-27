9b56562c3b4a7f6a4254dc3c3c1fe06e7b52c213
Musyahid Abror <you@example.com>
2020-08-28 03:29:29 +0800

    add lazy loading

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,28 @@
-import Home from './pages/Home.vue'
-import AlbumIndex from "./pages/Albums/Index";
-import PhotoIndex from "./pages/Photos/Index";
-import PostIndex from "./pages/Posts/Index";
-import Album from './pages/Albums/Album.vue'
-import Photo from './pages/Photos/Photo.vue'
-import Post from './pages/Posts/Post.vue'
-import AlbumDetails from './pages/Albums/AlbumDetails.vue'
-import PhotoDetails from './pages/Photos/PhotoDetails.vue'
-import PostsDetails from './pages/Posts/PostDetails.vue'
+// import Home from './pages/Home.vue'
+// import AlbumIndex from "./pages/Albums/Index";
+// import PhotoIndex from "./pages/Photos/Index";
+// import PostIndex from "./pages/Posts/Index";
+// import Album from './pages/Albums/Album.vue'
+// import Photo from './pages/Photos/Photo.vue'
+// import Post from './pages/Posts/Post.vue'
+// import AlbumDetails from './pages/Albums/AlbumDetails.vue'
+// import PhotoDetails from './pages/Photos/PhotoDetails.vue'
+// import PostsDetails from './pages/Posts/PostDetails.vue'
 
 const routes = [
-    { path: '/', component: Home },
+    { path: '/', component: () => import (/* webpackChunkName: "Home */  './pages/Home.vue') },
     { 
       path: '/album', 
-      component: AlbumIndex, 
+      component: () => import (/* webpackChunkName: "Album Index*/ './pages/Albums/Index'), 
       name: "albumIndex",
         children: [
           {
             path: "",
-            component: Album
+            component: () => import(/* webpackChunkName: "Album" */ './pages/Albums/Album.vue')
           },
           {
             path: "detail/:id",
-            component: AlbumDetails,
+            component: () => import (/* webpackChunkName: "Album Details" */ './pages/Albums/AlbumDetails.vue'),
             name: "albumDetail",
           }
 
@@ -30,16 +30,16 @@ const routes = [
       },
     { 
       path: '/photo', 
-        component: PhotoIndex,
+        component: () => import (/* webpackChunkName: "Photo Index" */ "./pages/Photos/Index"),
         name: "photoIndex",
         children: [
           {
             path: "",
-            component: Photo
+            component: () => import (/* webpackChunkName: "Photo" */ './pages/Photos/Photo.vue')
           },
           {
             path: "detail/:id",
-            component: PhotoDetails,
+            component: () => import (/* webpackChunkName: "PhotoDetails" */ './pages/Photos/PhotoDetails.vue'),
             name: "photoDetail",
           }
 
@@ -47,22 +47,22 @@ const routes = [
     },
     { 
       path: '/post', 
-        component: PostIndex,
+        component: () => import (/* webpackChunkName: "Post Index" */ "./pages/Posts/Index"),
         name: 'postIndex',
         children: [
           {
             path: "",
-            component: Post
+            component: () => import(/* webpackChunkName: "Post" */ './pages/Posts/Post.vue')
           },
           {
             path: "detail/:id",
-            component: PostsDetails,
+            component: () => import (/* webpackChunkName: "Post Details" */ './pages/Posts/PostDetails.vue'
+            ),
             name: "postDetail",
           }
 
         ] 
-    },
-    // { path: '/album/:id', component: AlbumDetails },
+    }
   ]
 
 export default routes;
